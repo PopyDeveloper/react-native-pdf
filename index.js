fd4f19d96aced119c225f5ad81054073809c6980
Rafael Nascimento <rafa.dev@hotmail.com>
2023-10-17 14:39:19 -0300

    remove

--- index.js
+++ index.js
@@ -412,7 +412,6 @@ export default class Pdf extends Component {
                                             style={[{flex:1,backgroundColor: '#EEE'}, this.props.style]}
                                             path={this.state.path}
                                             onChange={this._onChange}
-                                            onLoadComplete={this.props.onLoadComplete}
                                         />
                                     ):(
                                         this.props.usePDFKit ?(
